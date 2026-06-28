e035ef2af82b0952fcd70e3f27b2215996e83393
ootsumasaya <capibaratotoro@gmail.com>
2026-06-28 22:06:22 +0900

    Userのedit画面でprofileも更新できるようにする

--- app/fuel/app/views/user/edit.php
+++ app/fuel/app/views/user/edit.php
@@ -1,7 +1,18 @@
 <h2>Editing <span class='muted'>User</span></h2>
 <br>
+<?php if ( ! empty($errors)): ?>
+		<div class="alert alert-danger">
+				<ul>
+						<?php foreach ($errors as $error): ?>
+								<li><?php echo $error; ?></li>
+						<?php endforeach; ?>
+				</ul>
+		</div>
+<?php endif; ?>
 
-<?php echo render('user/_form'); ?>
-<p>
-	<?php echo Html::anchor('user/view/'.$user->id, 'View'); ?> |
-	<?php echo Html::anchor('user', 'Back'); ?></p>
+<?php echo Form::open(array("class"=>"form-horizontal")); ?>
+	<?php echo Fieldset::forge('user')->add_model($user)->populate($user, true)->build(); ?>
+<?php echo Form::close(); ?>
+
+<?php echo Html::anchor('user/view/'.$user->id, 'View'); ?> |
+<?php echo Html::anchor('user', 'Back'); ?></p>
